4b3070d854309a87addb9653e2de793355c6f80a
reza_azimi <reza.azimi@inferencesolutions.com>
2023-08-17 23:27:57 +1000

    hpdate app to show pages

--- app.py
+++ app.py
@@ -20,22 +20,24 @@ app.layout = html.Div([
     html.Div(id='page-content')
 ])
 
-login_layout = html.Div([
-    html.H1('Login'),
-    html.Div([
-        dcc.Input(id='username-input', placeholder='Username', type='text', style={'marginRight': '10px'}),
-        dcc.Input(id='password-input', placeholder='Password', type='password', style={'marginRight': '10px'}),
-        html.Button('Login', id='login-button', n_clicks=0)
-    ], style={'marginBottom': '20px'}),
-    html.Div(id='login-result')
-])
+def login_layout():
+    return html.Div([
+        html.H1('Login'),
+        html.Div([
+            dcc.Input(id='username-input', placeholder='Username', type='text', style={'marginRight': '10px'}),
+            dcc.Input(id='password-input', placeholder='Password', type='password', style={'marginRight': '10px'}),
+            html.Button('Login', id='login-button', n_clicks=0)
+        ], style={'marginBottom': '20px'}),
+        html.Div(id='login-result')
+    ])
 
-main_layout = html.Div([
-    dcc.Dropdown(id='symbol-dropdown', options=[], placeholder='Select a symbol'),
-    html.Button('Scan', id='scan-button', n_clicks=0),
-    dcc.Graph(id='candlestick-graph'),
-    html.Div(id='backtest-result')
-])
+def main_layout():
+    return html.Div([
+        dcc.Dropdown(id='symbol-dropdown', options=[], placeholder='Select a symbol'),
+        html.Button('Scan', id='scan-button', n_clicks=0),
+        dcc.Graph(id='candlestick-graph'),
+        html.Div(id='backtest-result')
+    ])
 
 @app.callback(Output('page-content', 'children'),
               Input('url', 'pathname'),
@@ -45,12 +47,11 @@ main_layout = html.Div([
 def display_page(pathname, username, password):
     if pathname == '/':
         if username in VALID_USERNAME_PASSWORD_PAIRS and password == VALID_USERNAME_PASSWORD_PAIRS[username]:
-            return dcc.Location(pathname='/main', id='dummy')
+            return main_layout()
         else:
             return html.Div('Invalid credentials')
     elif pathname == '/main':
-        return main_layout
-
+        return main_layout()
 @app.callback(Output('symbol-dropdown', 'options'),
               Input('scan-button', 'n_clicks'),
               State('symbol-dropdown', 'options'),
